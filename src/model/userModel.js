cf83be4cff8723afe183ea19610fd7a481316d4f
alinecbsr <alinecbsr@gmail.com>
2020-09-07 20:57:29 -0300

    Implementing new schema

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const listSchema = new Schema({
+  title: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+})
+
 const UserSchema = new Schema({
   name: {
     type: String,
@@ -11,10 +21,17 @@ const UserSchema = new Schema({
     type: String,
     required: `Please enter the user's email`
   },
+  sexo: {
+    type: [{
+      type: String,
+      enum: ['M', 'F', 'Uninformed']
+    }]
+  },
   created_at: {
     type: Date,
     default: Date.now
-  }
+  },
+  list: [listSchema]
 })
 
 module.exports = mongoose.model('Users', UserSchema)
